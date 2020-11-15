5c4550844dbf5187d016836d0727037352d1cf42
Michael-75 <52245580+Michael-75@users.noreply.github.com>
2020-11-15 11:50:40 +0000

    Changed the folder \'specs\' to \'tests\'

--- part_2_code/tests/card_game_tests.py
+++ part_2_code/tests/card_game_tests.py
@@ -3,4 +3,6 @@ from src.card import Card
 from src.card_game import CardGame
 
 class TestCardGame:
-    pass
+    
+    def test_value_is_1(self):
+        self.assertEqual(1, self.check_for_ace(1))
